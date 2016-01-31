3c8a425965412a3296e0a54bebe3b6fec75836aa
minibugdev <minibugdev@gmail.com>
2016-02-01 01:26:51 +0700

    add .subscribeOn() & .observeOn() to every Observable

--- app/src/main/java/com/example/hellorx/MainActivity.java
+++ app/src/main/java/com/example/hellorx/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.hellorx;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.hellorx.model.PixaBay;
+import com.example.hellorx.model.Quote;
 import com.example.hellorx.service.API;
+import com.squareup.picasso.Picasso;
 import com.trello.rxlifecycle.components.support.RxAppCompatActivity;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
 public class MainActivity extends RxAppCompatActivity {
@@ -18,8 +25,9 @@ public class MainActivity extends RxAppCompatActivity {
     public static final String CATEGORY_MOVIES = "movies";
     public static final String CATEGORY_FAMOUS = "famous";
 
-    @Bind(R.id.quote_textview) TextView mQuoteTextView;
-    @Bind(R.id.quote_author)   TextView mQuoteAuthor;
+    @Bind(R.id.quote_textview) TextView  mQuoteTextView;
+    @Bind(R.id.quote_author)   TextView  mQuoteAuthor;
+    @Bind(R.id.quote_image)    ImageView mQuoteImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,13 +59,68 @@ public class MainActivity extends RxAppCompatActivity {
     }
 
     private void getQuote() {
-        API.quote().getQuote(CATEGORY_MOVIES)
-            .flatMap(quote -> API.image().getPhoto(quote.getAuthor()))
-            .filter(pixaBay -> pixaBay.getTotalImages() > 0)
-            .map(pixaBay -> pixaBay.getImages().get(0).getImageURL())
+        API.quote().getQuote(CATEGORY_FAMOUS)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(url -> mQuoteAuthor.setText(url));
+            .doOnNext(quote -> {
+                mQuoteTextView.setText(quote.getQuote());
+                mQuoteAuthor.setText(quote.getAuthor());
+            })
+            .flatMap(quote ->
+                    API.image().getPhoto(quote.getAuthor())
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+            )
+            .subscribe(image -> {
+                if (image != null && image.getTotalImages() > 0) {
+                    mQuoteImage.setVisibility(View.VISIBLE);
+                    Picasso.with(getBaseContext())
+                        .load(image.getImages().get(0).getImageURL())
+                        .fit()
+                        .centerCrop()
+                        .noFade()
+                        .into(mQuoteImage);
+                }
+                else {
+                    mQuoteImage.setVisibility(View.GONE);
+                }
+            }, e -> {
+                mQuoteImage.setVisibility(View.GONE);
+                Log.e(TAG, e.getMessage());
+            });
+    }
+
+    private void getQuoteCombind() {
+        API.quote().getQuote(CATEGORY_FAMOUS)
+            .flatMap(quote -> API.image().getPhoto(quote.getAuthor())
+                    .map((Func1<PixaBay, Quote>) pixaBay -> {
+                        if (pixaBay.getTotalImages() > 0) {
+                            quote.setAuthorImage(pixaBay.getImages().get(0));
+                        }
+                        return quote;
+                    })
+            )
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(quote -> {
+                mQuoteTextView.setText(quote.getQuote());
+                mQuoteAuthor.setText(quote.getAuthor());
+
+                if (quote.getAuthorImage() != null) {
+                    mQuoteImage.setVisibility(View.VISIBLE);
+                    Picasso.with(getBaseContext())
+                        .load(quote.getAuthorImage().getImageURL())
+                        .fit()
+                        .centerCrop()
+                        .into(mQuoteImage);
+                }
+                else {
+                    mQuoteImage.setVisibility(View.GONE);
+                }
+            }, e -> {
+                mQuoteImage.setVisibility(View.GONE);
+                Log.e(TAG, e.getMessage());
+            });
     }
 }
 
--- app/src/main/java/com/example/hellorx/model/Quote.java
+++ app/src/main/java/com/example/hellorx/model/Quote.java
@@ -2,8 +2,9 @@ package com.example.hellorx.model;
 
 public class Quote {
     private String quote;
-    private String author;
     private String category;
+    private String author;
+    private Image  authorImage;
 
     public String getQuote() {
         return quote;
@@ -15,5 +16,25 @@ public class Quote {
 
     public String getCategory() {
         return category;
+    }
+
+    public Image getAuthorImage() {
+        return authorImage;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setAuthorImage(Image authorImage) {
+        this.authorImage = authorImage;
     }
 }
